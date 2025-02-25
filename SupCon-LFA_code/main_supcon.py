d59c3b8214fddf5c3d80a164a947cdc15574639b
eleven <poppywolf983@outlook.com>
2025-02-25 09:40:47 +0800

    Create main_supcon.py

---
+++ SupCon-LFA_code/main_supcon.py
@@ -0,0 +1,212 @@
+from __future__ import print_function
+
+import os, sys, argparse, time, math
+
+import tensorboard_logger as tb_logger
+import torch
+import torch.backends.cudnn as cudnn
+from torchvision import transforms, datasets
+from util import TwoCropTransform, AverageMeter
+from util import adjust_learning_rate
+from util import set_optimizer, save_model
+from util import SupConResNet,SupConLoss
+
+try:
+    import apex
+    from apex import amp, optimizers
+except ImportError:
+    pass
+
+
+def parse_option():
+    parser = argparse.ArgumentParser('argument for training')
+
+    parser.add_argument('--print_freq', type=int, default=10,
+                        help='print frequency')
+    parser.add_argument('--save_freq', type=int, default=10,
+                        help='save frequency')
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help='batch_size')
+    parser.add_argument('--num_workers', type=int, default=16,
+                        help='num of workers to use')
+    parser.add_argument('--epochs', type=int, default=60,
+                        help='number of training epochs')
+    # optimization
+    parser.add_argument('--learning_rate', type=float, default=0.005,
+                        help='learning rate')
+    parser.add_argument('--lr_decay_epochs', type=str, default='15,30,45',
+                        help='where to decay lr, can be a list')
+    parser.add_argument('--lr_decay_rate', type=float, default=0.1,
+                        help='decay rate for learning rate')
+    parser.add_argument('--weight_decay', type=float, default=1e-4,
+                        help='weight decay')
+    parser.add_argument('--momentum', type=float, default=0.9,
+                        help='momentum')
+    # model dataset
+    parser.add_argument('--model', type=str, default='resnet50')
+    parser.add_argument('--dataset', type=str, default='path',
+                        choices=['path'], help='dataset')
+    parser.add_argument('--mean', type=str, help='mean of dataset in path in form of str tuple')
+    parser.add_argument('--std', type=str, help='std of dataset in path in form of str tuple')
+    parser.add_argument('--data_folder', type=str, default=None, help='path to custom dataset')
+    parser.add_argument('--size', type=int, default=448, help='parameter for Resized')
+    # method
+    parser.add_argument('--method', type=str, default='SupCon',help='choose method')
+    # temperature
+    parser.add_argument('--temp', type=float, default=0.3,
+                        help='temperature for loss function')
+    # other setting
+    parser.add_argument('--trial', type=str, default='0',
+                        help='id for recording multiple runs')
+    opt = parser.parse_args()
+    # check if dataset is path that passed required arguments
+    if opt.dataset == 'path':
+        assert opt.data_folder is not None \
+            and opt.mean is not None \
+            and opt.std is not None
+
+    opt.model_path = './save/SupCon/{}_models'.format(opt.dataset)
+    opt.tb_path = './save/SupCon/{}_tensorboard'.format(opt.dataset)
+    iterations = opt.lr_decay_epochs.split(',')
+    opt.lr_decay_epochs = list([])
+    for it in iterations:
+        opt.lr_decay_epochs.append(int(it))
+    opt.model_name = '{}_{}_{}_lr_{}_decay_{}_bsz_{}_temp_{}_trial_{}'.\
+        format(opt.method, opt.dataset, opt.model, opt.learning_rate,
+               opt.weight_decay, opt.batch_size, opt.temp, opt.trial)
+
+    opt.tb_folder = os.path.join(opt.tb_path, opt.model_name)
+    if not os.path.isdir(opt.tb_folder):
+        os.makedirs(opt.tb_folder)
+    opt.save_folder = os.path.join(opt.model_path, opt.model_name)
+    if not os.path.isdir(opt.save_folder):
+        os.makedirs(opt.save_folder)
+    return opt
+
+def set_loader(opt):
+    if opt.dataset == 'path':
+        mean = eval(opt.mean)
+        std = eval(opt.std)
+    else:
+        raise ValueError('dataset not supported: {}'.format(opt.dataset))
+    normalize = transforms.Normalize(mean=mean, std=std)
+    train_transform = transforms.Compose([
+        transforms.Resize([opt.size,opt.size]),
+        transforms.Grayscale(num_output_channels=3),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    if opt.dataset == 'path':
+        train_dataset = datasets.ImageFolder(root=opt.data_folder,
+                                            transform=TwoCropTransform(train_transform))
+    else:
+        raise ValueError(opt.dataset)
+    train_sampler = None
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=opt.batch_size, shuffle=(train_sampler is None),
+        num_workers=opt.num_workers, pin_memory=True, sampler=train_sampler)
+    return train_loader
+
+
+def set_model(opt):
+    model = SupConResNet(name=opt.model)
+    criterion = SupConLoss(temperature=opt.temp)
+    if torch.cuda.is_available():
+        if torch.cuda.device_count() > 1:
+            model.encoder = torch.nn.DataParallel(model.encoder)
+        model = model.cuda()
+        criterion = criterion.cuda()
+        cudnn.benchmark = True
+    return model, criterion
+
+def train(train_loader, model, criterion, optimizer, epoch, opt):
+    model.train()
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    end = time.time()
+    
+    for idx, (images, labels) in enumerate(train_loader):
+        data_time.update(time.time() - end)
+        images = torch.cat([images[0], images[1]], dim=0)
+        if torch.cuda.is_available():
+            images = images.cuda(non_blocking=True)          
+            labels = labels.cuda(non_blocking=True)
+        bsz = labels.shape[0]
+        # compute loss
+        features = model(images)
+        f1, f2 = torch.split(features, [bsz, bsz], dim=0)
+        features = torch.cat([f1.unsqueeze(1), f2.unsqueeze(1)], dim=1)
+        if opt.method == 'SupCon':
+            loss = criterion(features, labels)
+        else:
+            raise ValueError('contrastive method not supported: {}'.
+                             format(opt.method))
+
+        # update metric
+        losses.update(loss.item(), bsz)
+
+        # SGD
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # print info
+        if (idx + 1) % opt.print_freq == 0:
+            print('Train: [{0}][{1}/{2}]\t'
+                  'BT {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'DT {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'loss {loss.val:.3f} ({loss.avg:.3f})'.format(
+                   epoch, idx + 1, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses))
+            sys.stdout.flush()
+
+    return losses.avg
+
+
+def main():
+    opt = parse_option()
+
+    # build data loader
+    train_loader = set_loader(opt)
+
+    # build model and criterion
+    model, criterion = set_model(opt)
+    # build optimizer
+    optimizer = set_optimizer(opt, model)
+
+    # tensorboard
+    logger = tb_logger.Logger(logdir=opt.tb_folder, flush_secs=2)
+
+    # training routine
+    for epoch in range(1, opt.epochs + 1):
+        adjust_learning_rate(opt, optimizer, epoch)
+
+        # train for one epoch
+        time1 = time.time()
+        loss = train(train_loader, model, criterion, optimizer, epoch, opt)
+        time2 = time.time()
+        print('epoch {}, total time {:.2f}'.format(epoch, time2 - time1))
+
+        # tensorboard logger
+        logger.log_value('loss', loss, epoch)
+        logger.log_value('learning_rate', optimizer.param_groups[0]['lr'], epoch)
+
+        if epoch % opt.save_freq == 0:
+            save_file = os.path.join(
+                opt.save_folder, 'ckpt_epoch_{epoch}.pth'.format(epoch=epoch))
+            save_model(model, optimizer, opt, epoch, save_file)
+
+    # save the last model
+    save_file = os.path.join(
+        opt.save_folder, 'last.pth')
+    save_model(model, optimizer, opt, opt.epochs, save_file)
+
+
+if __name__ == '__main__':
+    main()
